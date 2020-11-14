d12a816855f36ca4c0270701ade6888e65ab8ced
Sudhakar Patnaik <sudhakarpartnaik.mca2009@gmail.com>
2020-11-14 18:40:42 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+
+function App() {
+  return (
+    <div>
+    <h1 className='App-header'>Welcome to my new App</h1>
+    </div>
+  );
+}
+
+export default App;
